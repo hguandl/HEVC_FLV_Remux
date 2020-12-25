a56a409db48581ce7c34e8abae0fb2e281abb9df
hguandl <hguandl@gmail.com>
2020-12-25 18:12:27 +0800

    Correct connection timeout by microseconds

--- src/remux.c
+++ src/remux.c
@@ -54,7 +54,7 @@ int remux(const char *in_filename, const char *out_filename, const char *http_he
     av_log_set_level(AV_LOG_WARNING);
 
     if (http_headers) {
-        av_dict_set(&options, "timeout", "5000", AV_DICT_APPEND);
+        av_dict_set(&options, "timeout", "5000000", AV_DICT_APPEND);
         av_dict_set(&options, "headers", http_headers, AV_DICT_APPEND);
         av_dict_set(&options, "multiple_requests", "1", AV_DICT_APPEND);
         av_dict_set(&options, "reconnect_at_eof", "1", AV_DICT_APPEND);
